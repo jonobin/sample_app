38a4e5a4c7f44f2bc0613e86ccb7d652056de465
J Boss <jonkazama@gmail.com>
2011-11-06 23:00:35 -0800

    Added help test and dynamic base title in pages spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ Spork.prefork do
     ### Part of a Spork hack. See http://bit.ly/arY19y
     # Emulate initializer set_clear_dependencies_hook in
     # railties/lib/rails/application/bootstrap.rb
-    #ActiveSupport::Dependencies.clear
+    ActiveSupport::Dependencies.clear
   end
 end
 
