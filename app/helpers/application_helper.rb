63ee9b41e56ff61ae2e90117c244b0afa2ecc5cf
J Boss <jonkazama@gmail.com>
2011-11-07 22:48:38 -0800

    Added sample user rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return a title on a per-page basis.
+  def title                                                 # Documentation comment
+    base_title = "Ruby on Rails Tutorial Sample App"        # Method definition
+    if @title.nil?                                          # Variable assignment
+      base_title                                            # Boolean test for nil
+    else                                                    # Implicit return
+      "#{base_title} | #{@title}"                           # String interpolation
+    end
+  end
 end
