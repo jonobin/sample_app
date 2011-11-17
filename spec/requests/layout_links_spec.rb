a366d6fbc7369ee2f3afac80699de99fedda1135
J Boss <jonkazama@gmail.com>
2011-11-16 20:22:17 -0800

    completed integration tests for layout links chap 5

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,4 +25,30 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+  
+  it "should have the right links on the layout" do
+    
+    #visits the root path page
+    visit root_path
+    
+    #on the root page, simulate a click to the About link, which should visit the destination page
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    
+    #on the root page, simulate a click to the About link, which should visit the destination page
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    
+    #on the root page, simulate a click to the About link, which should visit the destination page
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    
+    #on the root page, simulate a click to the About link, which should visit the destination page
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    
+    #on the root page, simulate a click to the About link, which should visit the destination page
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
