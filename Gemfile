231e427e605ac7bdbde45e3516527ba79cb03944
J Boss <jonkazama@gmail.com>
2011-11-27 01:51:42 -0800

    Done with user edit/update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -9,12 +9,14 @@ gem 'sqlite3'
 gem 'sqlite3-ruby', '1.3.1', :require => 'sqlite3'
 gem 'gravatar_image_tag', '0.1.0'
 gem 'pg'
+gem 'will_paginate', '3.0.pre2'
 
 group :development do
 	gem 'autotest'
 	gem 'rspec'
 	gem "rspec-rails" 
 	gem 'annotate', :git => 'git://github.com/ctran/annotate_models.git'
+	gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  # current_user is only created here so we need to add :destroy here
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+  
   def new
     @user = User.new
     @title = "Sign up"
@@ -22,5 +32,40 @@ class UsersController < ApplicationController
       @user.password_confirmation.clear
       render 'new'
     end
+  end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+  private
+  
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
+  
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
